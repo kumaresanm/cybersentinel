a1712fcef4e390f688db7264a8285c65cfddb3ef
kumaresan.muthaliar <kumaresan.muthaliar@avalara.com>
2023-08-30 08:47:57 +0530

    First version

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { DefaultModule } from './layouts/default/default.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DefaultModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
